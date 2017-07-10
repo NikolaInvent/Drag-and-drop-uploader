f5e2218275faeb1c939ab694fb40e61d661cf716
NikolaInvent <n.cavic11@gmail.com>
2017-07-11 01:59:52 +0200

    first commit

---
+++ DragDrop/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DragDrop.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFiles(IEnumerable<HttpPostedFileBase> files)
+        {
+            foreach (var file in files)
+            {
+                string filePath = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                file.SaveAs(Path.Combine(Server.MapPath("~/UploadedFiles"), filePath));
+            }
+            return Json("file uploaded successfully");
+        }
+    }
+}
